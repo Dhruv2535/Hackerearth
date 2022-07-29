70ff8f97ab16a0f5ca49c5bf6a71351f80859024
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-07-29 13:37:54 +0530

    Add files via upload

--- Basic Programming/Basics of Input_Output/Roy and Profile Picture.cpp
+++ Basic Programming/Basics of Input_Output/Roy and Profile Picture.cpp
@@ -1,5 +1,5 @@
 //Author: Akash Gautam (@geekblower)
-//Date: 02-07-2022
+//Date: 28-07-2022
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -20,26 +20,76 @@ using namespace std;
 #define create(n) int n;cin>>n;
 #define def(a,n) int a[n];start_loop cin>>a[i];
 #define print(a) for(auto i : a){cout<<i<<" ";}cout<<endl;
-const int MOD = 1e9+7;
+#define vector(a,n) VI a;loop(i,0,n,1){int x;cin>>x;a.PB(x);}
+inline int GCD(int a,int b){return !b?a:GCD(b,a%b);}
+const int MOD = 1000000007;
 
-
-void solve(int l) {
-    create(w)
-    create(h)
+void solve() {
+    create(l)
+    create(n)
     
-    if(w<l || h<l)
-        display("UPLOAD ANOTHER")
-    else if(w==l && h==l)
-        display("ACCEPTED")
-    else
-        display("CROP IT")
-}
+    while(n--) {
+        create(w)a//Author: Akash Gautam (@geekblower)
+//Date: 28-07-2022
+#include <bits/stdc++.h>
+using namespace std;
 
+#define endl '\n'
+#define GEEKBLOWER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define int long long int
+#define TEST_CASES create(t);while(t--)
+#define SOLUTION solve(); return 0;
+#define display(n) cout<<n<<endl;
+#define VI vector<int>
+#define PB push_back
+#define ALL(a) a.begin(),a.end()
+#define SORT(a) sort(ALL(a))
+#define loop(i,a,b,c) for(int i=a;i<b;i+=c)
+#define revloop(i,a,b,c) for(int i=a;i>=b;i-=c)
+#define start_loop loop(i,0,n,1)
+#define start_revloop revloop(i,n,0,1)
+#define create(n) int n;cin>>n;
+#define def(a,n) int a[n];start_loop cin>>a[i];
+#define print(a) for(auto i : a){cout<<i<<" ";}cout<<endl;
+#define vector(a,n) VI a;loop(i,0,n,1){int x;cin>>x;a.PB(x);}
+inline int GCD(int a,int b){return !b?a:GCD(b,a%b);}
+const int MOD = 1000000007;
+
+void solve() {
+    create(l)
+    create(n)
+    
+    while(n--) {
+        create(w)
+        create(h)
+        
+        if(w<l || h<l)
+            display("UPLOAD ANOTHER")
+        else if(w!=h)
+            display("CROP IT")
+        else
+            display("ACCEPTED")
+    }
+}
 
 int32_t main() {
     GEEKBLOWER
-    create(l)
+    SOLUTION
+}
+
+        create(h)
+        
+        if(w<l || h<l)
+            display("UPLOAD ANOTHER")
+        else if(w>l || h>l)
+            display("CROP IT")
+        else
+            display("ACCEPTED")
+    }
+}
+
+int32_t main() {
+    GEEKBLOWER
     TEST_CASES
-        solve(l);
-    return 0;
+    SOLUTION
 }
